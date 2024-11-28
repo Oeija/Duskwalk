98a118193c95d44f2a52334ee8229fecfde733f7
Vincent Oei <oei.vincent20@gmail.com>
2024-11-28 20:49:11 +0700

    undo commit

--- fe/src/components/Input.jsx
+++ fe/src/components/Input.jsx
@@ -11,7 +11,7 @@ function Input(props) {
                     value={todoValue} 
                     onChange={(e) => setTodoValue(e.target.value)} 
                     placeholder="Mark your trail ahead..."
-                    className="text-lg sm:text-sm text-gray-100 bg-gradient-to-b from-green-900 to-gray-800 border-2 border-gold px-6 py-3 focus:outline-none focus:ring-4 focus:ring-yellow-500 shadow-lg shadow-gray-800 transition-all ease-in-out"
+                    className="text-lg text-gray-100 bg-gradient-to-b from-green-900 to-gray-800 border-2 border-gold px-6 py-3 focus:outline-none focus:ring-4 focus:ring-yellow-500 shadow-lg shadow-gray-800 transition-all ease-in-out"
                     style={{
                         clipPath: "polygon(10% 0%, 90% 0%, 100% 10%, 100% 90%, 90% 100%, 10% 100%, 0% 90%, 0% 10%)", 
                     }}
@@ -24,7 +24,7 @@ function Input(props) {
                         handleAddTodos(todoValue);
                         setTodoValue('');
                     }}
-                    className="text-lg sm:text-sm text-gray-200 bg-gradient-to-b from-orange-700 to-gray-800 border-2 border-gold px-8 py-3 focus:outline-none focus:ring-4 focus:ring-yellow-500 hover:bg-yellow-400 transition-all ease-in-out"
+                    className="text-lg text-gray-200 bg-gradient-to-b from-orange-700 to-gray-800 border-2 border-gold px-8 py-3 focus:outline-none focus:ring-4 focus:ring-yellow-500 hover:bg-yellow-400 transition-all ease-in-out"
                     style={{
                         clipPath: "polygon(10% 0%, 90% 0%, 100% 10%, 100% 90%, 90% 100%, 10% 100%, 0% 90%, 0% 10%)",
                     }}
@@ -37,7 +37,7 @@ function Input(props) {
                 </motion.button>
             </div>
             <div className="flex justify-center items-center">
-                <p className="text-base sm:text-sm">Hearken: Thy quests shall endure, even shouldst thou refresh thy page.</p>
+                <p>Hearken: Thy quests shall endure, even shouldst thou refresh thy page.</p>
             </div>
         </div>
         
