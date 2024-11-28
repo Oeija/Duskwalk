c6e945ccade93c1e9cc99c8f6bdc43c8de9275bd
Vincent Oei <oei.vincent20@gmail.com>
2024-11-28 19:34:56 +0700

    update mobile view

--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div>
         <div className="flex flex-col justify-center items-center m-16">
             <img src={logo} alt="logo" className="w-[80%] max-w-[600px] h-auto object-contain sm:w-[100%] lg:max-w-[600px] mb-3" />
-            <p className='text-2xl font-semibold sm:text-xl lg:text-3xl'>chart your path, complete your quest</p>
+            <p className='text-xl font-semibold justify-center sm:text-2xl lg:text-3xl'>chart your path, complete your quest</p>
         </div>
     </div>
   )
